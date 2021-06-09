bef6a7d1f9432b27788dd9118ffb2ef50fc16cef
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-06-09 15:28:53 -0500

    Add subject and name parameters

--- server/schema/__tests__/contactme.test.ts
+++ server/schema/__tests__/contactme.test.ts
@@ -1,36 +1,51 @@
 import { mocked } from "ts-jest/utils";
 
+import loadMailTemplate from "../../utils/loadMailTemplate";
 import transporter from "../../utils/mailTransporter";
 
 import { contactMeResolvers } from "../contactme";
 
 jest.mock("../../config", () => ({
   MY_EMAIL: "myemail@example.com",
-  MAIL_CONFIG: {}
+  MAIL_CONFIG: {},
+  WEBSITE_URL: "testwebsiteurl"
 }))
 
 jest.mock("../../utils/mailTransporter");
+jest.mock("../../utils/loadMailTemplate");
 
 const mockedTransporterSendMail = mocked(transporter.sendMail);
+const mockedLoadMailTemplate = mocked(loadMailTemplate);
 
 describe("server/schema/contactme", () => {
   beforeEach(() => {
     mockedTransporterSendMail.mockReset();
+    mockedLoadMailTemplate.mockReset();
+
+    mockedLoadMailTemplate.mockImplementation(() => "test template");
   });
 
   it("should call transporter.sendMail correctly", () => {
     const result = contactMeResolvers.Mutation.sendMessage(null, {
+      name: "testname",
       email: "test@example.com",
+      subject: "testsubject",
       message: "test message"
     })
 
     expect(result).toBeTruthy();
 
+    expect(mockedLoadMailTemplate).toHaveBeenCalledWith("sendMessage", {
+      websiteURL: "testwebsiteurl",
+      name: "testname",
+      message: "test message"
+    });
+
     expect(mockedTransporterSendMail).toHaveBeenCalledWith({
       from: "test@example.com",
       to: "myemail@example.com",
-      subject: "Fernando Vaca Tamayo - A new message from test@example.com",
-      text: "test message"
+      subject: "Fernando Vaca Tamayo - testsubject",
+      html: "test template"
     });
   });
 });
--- server/schema/contactme.ts
+++ server/schema/contactme.ts
@@ -1,26 +1,35 @@
 import { gql } from "apollo-server-express";
 
-import { MY_EMAIL } from "../config";
+import { MY_EMAIL, WEBSITE_URL } from "../config";
 
+import loadMailTemplate from "../utils/loadMailTemplate";
 import transporter from "../utils/mailTransporter";
 
 export const ContactMe = gql`
   extend type Mutation {
-    sendMessage(email: String!, message: String!): Boolean
+    sendMessage(name: String!, email: String!, subject: String!, message: String!): Boolean
   }
 `;
 
 interface Parameters {
+  name: string,
   email: string,
+  subject: string,
   message: string
 }
 
 const sendMessage = async (_: null, args: Parameters) => {
+  let mailTemplate = loadMailTemplate("sendMessage", {
+    name: args.name,
+    websiteURL: WEBSITE_URL,
+    message: args.message
+  });
+
   await transporter.sendMail({
     from: args.email,
     to: MY_EMAIL,
-    subject: `Fernando Vaca Tamayo - A new message from ${args.email}`,
-    text: args.message
+    subject: `Fernando Vaca Tamayo - ${args.subject}`,
+    html: mailTemplate
   });
 
   return true;
