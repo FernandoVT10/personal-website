a54c56b45ead285068f3030c143a97b6b5eb8075
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-07-08 16:46:13 -0500

    Change the projects filter logic in a new file

--- src/domain/Projects/Projects.tsx
+++ src/domain/Projects/Projects.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-
-import { useRouter } from "next/router";
+import React from "react";
 
 import { gql, useLazyQuery } from "@apollo/client";
 
@@ -8,8 +6,9 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ProjectList from "@/components/Projects/ProjectList";
 import Pagination, { PAGINATION_PROPS } from "@/components/Pagination";
-
 import Filters from "@/components/Projects/ProjectsFilter";
+
+import useProjectsFilter, { IVariables } from "@/hooks/useProjectsFilter";
 
 import styles from "./Projects.module.scss";
 
@@ -35,55 +34,28 @@ export const GET_TECHNOLOGIES = gql`
   }
 `;
 
-const getVariables = (): { page: number, search: string, technology: string } => {
-  const query = new URLSearchParams(window.location.search);
-
-  const page = query.get("page") ? parseInt(query.get("page")) || 0 : 0;
-  const search = query.get("search") ?? "";
-  const technology = query.get("technology") ?? "";
-
-  return { page, search, technology }
-}
-
 const Projects = () => {
-  const [search, setSearch] = useState("");
-  const [selectedTechnology, setSelectedTechnology] = useState("");
-
   const [getProjects, projectsResult] = useLazyQuery(GET_PROJECTS);
   const [getTechnologies, technologiesResult] = useLazyQuery(GET_TECHNOLOGIES);
 
-  const router = useRouter();
-
-  useEffect(() => {
-    getProjects({ variables: getVariables() });
-    getTechnologies();
-  }, []);
-
-  useEffect(() => {
-    const variables = getVariables();
-
-    // if the query has been called and isn't loading, we're going to refetch the data with the new variables
-    if(projectsResult.called && !projectsResult.loading) {
-      projectsResult.refetch(variables);
+  const toTheChangeOfVariables = (newVariables: IVariables) => {
+    if(!projectsResult.called) {
+      getProjects({ variables: newVariables });
+      getTechnologies();
     }
 
-    setSearch(variables.search);
-    setSelectedTechnology(variables.technology);
-  }, [router.query]);
-
-  const handleOnSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const query: { [key: string]: string } = {}
-
-    if(search) query.search = search;
-    if(selectedTechnology) query.technology = selectedTechnology;
-
-    router.push({
-      pathname: router.pathname,
-      query
-    });
+    if(projectsResult.called && !projectsResult.loading) {
+      projectsResult.refetch(newVariables);
+    }
   }
+
+  const {
+    search,
+    setSearch,
+    selectedTechnology,
+    setSelectedTechnology,
+    handleOnSubmit
+  } = useProjectsFilter(toTheChangeOfVariables);
 
   return (
     <div className={styles.projects}>
