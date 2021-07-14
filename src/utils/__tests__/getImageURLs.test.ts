212e1580ef61137fdb4be2e978a3cb7030bb39e2
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-07-14 14:10:23 -0500

    Add tests to getImageURLs and validators files

---
+++ src/utils/__tests__/getImageURLs.test.ts
@@ -0,0 +1,69 @@
+import getImageURLs from "../getImageURLs";
+
+const FILES_MOCK = [
+  new File([], "test 1", { type: "image/png" }),
+  new File([], "test 2", { type: "image/jpg" }),
+  new File([], "test 3", { type: "image/jpeg" })
+];
+
+const readAsDataURLMock = jest.fn();
+
+class FileReaderMock {
+  onload: Function
+
+  readAsDataURL(file: File) {
+    this.onload({
+      target: {
+        result: {
+          toString: () => `https://${file.name}.jpg`
+        }
+      }
+    });
+
+    readAsDataURLMock(file);
+  }
+}
+
+
+
+describe("src/utils/getImageURLs", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    window.FileReader.prototype.readAsDataURL = function (file: File) {
+      this.onload({
+        target: {
+          result: {
+            toString: () => `https://${file.name}.jpg`
+          }
+        }
+      });
+
+      readAsDataURLMock(file);
+    }
+  });
+
+  it("should get the image urls correctly", async () => {
+    expect(await getImageURLs(FILES_MOCK)).toEqual([
+      "https://test 1.jpg",
+      "https://test 2.jpg",
+      "https://test 3.jpg"
+    ]);
+
+    FILES_MOCK.forEach(file => expect(readAsDataURLMock).toHaveBeenCalledWith(file));
+
+    expect(readAsDataURLMock).toHaveBeenCalledTimes(3);
+  });
+
+  it("should return an error", async () => {
+    window.FileReader.prototype.readAsDataURL = function (_: File) {
+      this.onerror(new Error("test error"));
+    }
+
+    try {
+      await getImageURLs(FILES_MOCK);
+    } catch(err) {
+      expect(err).toEqual(new Error("test error"));
+    }
+  });
+});
--- src/utils/__tests__/validators.test.ts
+++ src/utils/__tests__/validators.test.ts
@@ -1,6 +1,18 @@
-import { email } from "../validators";
+import { email,  imageValidator } from "../validators";
 
 describe("src/utils/validators", () => {
+  describe("image validator", () => {
+    it("should return true when the image type is jpg, jpeg or png", () => {
+      ["image/jpg", "image/jpeg", "image/png"].forEach(imageType => {
+        expect(imageValidator(imageType)).toBeTruthy();
+      });
+    });
+
+    it("should return false when the image type is not allowed", () => {
+      expect(imageValidator("image/webp")).toBeFalsy();
+    });
+  });
+
   describe("email validator", () => {
     it("should return an error when the email is invalid", () => {
       expect(email("test@gc")).toBe("The email is invalid");
