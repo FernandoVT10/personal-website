3594a5a52980a65c498ef5e83af168cbbb17080e
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-08-03 15:09:32 -0500

    Add the tests to the uploadImage schema

---
+++ server/schema/__tests__/uploadImage.test.ts
@@ -0,0 +1,44 @@
+import { AuthenticationError } from "apollo-server-errors";
+import { mocked } from "ts-jest//utils";
+
+import { UploadImageResolvers } from "../uploadImage";
+
+import ImageController from "../../utils/controllers/ImageController";
+
+jest.mock("../../utils/controllers/ImageController");
+
+const IMAGE_MOCK = {
+  promise: Promise.resolve({
+    filename: "test name",
+    mimetype: "image/jpg"
+  })
+} as any;
+
+const uploadFileUploadAsImageMocked = mocked(ImageController.uploadFileUploadAsImage);
+
+describe("server/schema/uploadImage", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should return the image url", async () => {
+    uploadFileUploadAsImageMocked.mockResolvedValue("https://example.com/image.jpg");
+
+    expect(
+      await UploadImageResolvers.Mutation.uploadImage(null, { image: IMAGE_MOCK }, { loggedIn: true })
+    ).toBe("https://example.com/image.jpg");
+
+    expect(uploadFileUploadAsImageMocked).toHaveBeenCalledWith({
+      filename: "test name",
+      mimetype: "image/jpg"
+    }, "/content/");
+  });
+
+  it("should throw an error when the user isn't logged in", async () => {
+    try {
+      await UploadImageResolvers.Mutation.uploadImage(null, { image: null }, { loggedIn: false })
+    } catch (err) {
+      expect(err).toEqual(new AuthenticationError("You don't have enough permissions"));
+    }
+  });
+});
