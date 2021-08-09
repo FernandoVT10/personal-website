d41a6be8d71dcd9eda13b54e482298313da859f5
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-08-09 12:14:28 -0500

    Update the tests of the validators

--- src/utils/__tests__/validators.test.ts
+++ src/utils/__tests__/validators.test.ts
@@ -1,4 +1,4 @@
-import { email,  imageValidator } from "../validators";
+import { email,  imageValidator, inputValidators } from "../validators";
 
 describe("src/utils/validators", () => {
   describe("image validator", () => {
@@ -14,16 +14,40 @@ describe("src/utils/validators", () => {
   });
 
   describe("email validator", () => {
-    it("should return an error when the email is invalid", () => {
-      expect(email("test@gc")).toBe("The email is invalid");
-      expect(email("test@gmail.s")).toBe("The email is invalid");
+    it("should return false when the email is invalid", () => {
+      expect(email("test@gc")).toBeFalsy();
+      expect(email("test@gmail.s")).toBeFalsy();
     });
 
-    it("should return null when the email is valid", () => {
-      expect(email("test@example.com")).toBeNull();
-      expect(email("test@example.es")).toBeNull();
-      expect(email("test_12@e.es")).toBeNull();
-      expect(email("test_12@super.example")).toBeNull();
+    it("should return true when the email is valid", () => {
+      expect(email("test@example.com")).toBeTruthy();
+      expect(email("test@example.es")).toBeTruthy();
+      expect(email("test_12@e.es")).toBeTruthy();
+      expect(email("test_12@super.example")).toBeTruthy();
+    });
+  });
+
+  describe("inputValidators", () => {
+    describe("email", () => {
+      it("should return a message when the email is invalid", () => {
+        expect(inputValidators.email("test@gmail.s")).toBe("The email is invalid");
+      });
+
+      it("shouldn't return a message when the email is valid", () => {
+        expect(inputValidators.email("test@gmail.com")).toBeNull();
+      });
+    });
+
+    describe("requiredInput", () => {
+      it("should return a message when the value is empty", () => {
+        const validator = inputValidators.requiredInput("test name");
+        expect(validator("")).toBe("The test name is required");
+      });
+
+      it("should return null when the value isn't empty", () => {
+        const validator = inputValidators.requiredInput("test name");
+        expect(validator("foo")).toBeNull();
+      });
     });
   });
 });
