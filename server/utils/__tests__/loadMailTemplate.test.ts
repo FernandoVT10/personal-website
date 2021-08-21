299e1d66c1dcee45d846ed6f66d32aa8d2c674b3
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-08-21 16:08:39 -0500

    Fix the tests file

--- server/utils/__tests__/loadMailTemplate.test.ts
+++ server/utils/__tests__/loadMailTemplate.test.ts
@@ -1,24 +1,8 @@
-import { readFileSync } from "fs";
-
-import { mocked } from "ts-jest/utils";
-
 import loadMailTemplate from "../loadMailTemplate";
-
-jest.mock("fs");
 
 const TEMPLATE_MOCK = "Hello this is a test created by {testValue} and {otherTestValue}";
 
-const mockedReadFileSync = mocked(readFileSync);
-
 describe("server/utils/loadMailTemplate", () => {
-  beforeEach(() => {
-    mockedReadFileSync.mockReset();
-
-    mockedReadFileSync.mockImplementation(() => ({
-      toString: () => TEMPLATE_MOCK
-    } as any));
-  });
-
   it("should return a template with the variables value correctly", () => {
     const template = loadMailTemplate(TEMPLATE_MOCK, {
       testValue: "jhon",
@@ -26,9 +10,5 @@ describe("server/utils/loadMailTemplate", () => {
     });
 
     expect(template).toBe("Hello this is a test created by jhon and eric");
-
-    const mockCall = mockedReadFileSync.mock.calls[0];
-
-    expect(mockCall[0]).toMatch("mailTemplates/test.html");
   });
 });
